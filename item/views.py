a49d58b3212fd78424eb72569c238a9470f7dcda
Mariam <Mariamhh98@hotmail.com>
2020-01-14 11:30:27 +0300

    done with view and template of create!

--- item/views.py
+++ item/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Item
+from .forms import ItemForm
 
 def list (request):
 	items = Item.objects.all()
@@ -9,3 +10,21 @@ def list (request):
 		"items": items,
 	}
 	return render(request, "list.html", context)
+
+
+def create(request):
+
+	form = ItemForm()
+	if request.method == "POST":
+		form = ItemForm(request.POST, request.FILES or None)
+		if form.is_valid():
+			item = form.save(commit = False)
+			name = item.name
+			item.save()
+			messages.success(request, "%s is Successfully Added" %name)
+			return redirect('item-list')
+		print (form.errors)
+	context = {
+		"form": form,
+	}
+	return render(request, 'create.html', context)
