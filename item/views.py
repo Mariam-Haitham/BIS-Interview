d850cd98ce5fca56e77b8c171d9828910db86fd9
Mariam <Mariamhh98@hotmail.com>
2020-01-14 11:20:39 +0300

    done with view for list and the template!

--- item/views.py
+++ item/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Item
+
+def list (request):
+	items = Item.objects.all()
+
+	context = {
+		"items": items,
+	}
+	return render(request, "list.html", context)
